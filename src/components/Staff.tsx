074cef1d5d7cfe92424a984b34b2ec3c28aa5713
Joel Plowright <joelyjoel@protonmail.com>
2021-03-21 23:33:53 +0000

    Staff class

---
+++ src/components/Staff.tsx
@@ -0,0 +1,69 @@
+import React, {useContext} from 'react';
+import {FunctionComponent, createContext} from 'react';
+
+export interface StaffProps {
+  /**
+   * y-position of the top line of the staff.
+   */
+  top: number;
+
+  /**
+   * y-position of the bottom line of the staff.
+   */
+  bottom: number;
+
+  /**
+   * left-most x-position where each line is drawn from.
+   */
+  left: number;
+
+  /**
+   * right-most x-position where each line is drawn to.
+   */
+  right: number;
+}
+
+export const StaffContext = createContext({
+  staffTop: 0,
+  staffBottom: 4,
+  staffLeft: 0,
+  staffRight: 0,
+  spaceHeight: 1,
+  yLine: (n: number) => n,
+  ySpace: (n: number) => n + 1/2,
+  yNote: (n: number) => n/2,
+})
+
+export const useStaffContext = () => useContext(StaffContext);
+
+export const Staff: FunctionComponent<StaffProps> = ({top, bottom, left, right, children}) => {
+
+  let spaceHeight = (bottom - top) / 4;
+  let yLine = (n: number) => top + n*spaceHeight;
+  let ySpace = (n: number) => yLine(n) + spaceHeight / 2;
+  let yNote = (n: number) => yLine(n/2);
+
+  return <g>
+    <g>
+      <line x1={left} x2={right} y1={yLine(0)} y2={yLine(0)} />,
+      <line x1={left} x2={right} y1={yLine(1)} y2={yLine(1)} />,
+      <line x1={left} x2={right} y1={yLine(2)} y2={yLine(2)} />,
+      <line x1={left} x2={right} y1={yLine(3)} y2={yLine(3)} />,
+      <line x1={left} x2={right} y1={yLine(4)} y2={yLine(4)} />,
+    </g>
+    <StaffContext.Provider value={{
+      staffTop: top,
+      staffBottom: bottom,
+      staffLeft: left,
+      staffRight: right,
+      spaceHeight,
+      yLine, 
+      ySpace,
+      yNote,
+    }}>
+      {children}
+    </StaffContext.Provider>
+  </g>
+};
+
+export default Staff;
